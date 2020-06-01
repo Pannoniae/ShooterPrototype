0f91de4e748c3a9a45ad733b7e4864d7d9ff2ca4
DmanHUN <dmanhun@outlook.hu>
2020-06-01 15:45:25 +0200

    Basic main menu

---
+++ Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+    public GameObject settingsMenu;
+
+    public void ButtonStart()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ButtonSettings() 
+    {
+        settingsMenu.SetActive(!settingsMenu.activeSelf);
+    }
+
+    public void ButtonExit() 
+    {
+        Application.Quit();
+    }
+}
