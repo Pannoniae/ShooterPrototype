341fbf37fd5c1e1d35442457a92d66e65a6f8402
RealMarioD <mariocraft.MC2003@gmail.com>
2020-06-02 21:41:24 +0200

    Your free trial of life has ended screen

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,23 +1,37 @@
-﻿using System;
+using System;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
     
     public int maxHP = 100;
     public int HP;
+    public PlayerMovement toDisableScript;
+    public GameObject deadObject;
 
     void Awake() {
         HP = maxHP;
+    }
+
+    void Start() {
+        toDisableScript = GameManager.instance.player.GetComponent<PlayerMovement>();
     }
 
     void Update() {
         if (Input.GetKeyDown(KeyCode.G)) {
             damage(10);
         }
+        if (HP <= 0) {
+            deadObject.SetActive(true);
+            toDisableScript.enabled = false;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
     }
 
     public void damage(int dmg) {
         HP -= dmg;
+        if (HP < 0) HP = 0;
         GameManager.instance.hpText.text = HP.ToString();
         // TODO fancy damage effect lol
     }
