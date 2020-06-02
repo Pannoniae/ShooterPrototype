43ed69a6765f15520c78d0e84f631ba4fa5bf940
Pannoniae <prisznyak.zsombor@gmail.com>
2020-06-02 01:04:22 +0100

    - IT FUCKING WORKS

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
@@ -26,10 +27,18 @@ public class PlayerMovement : MonoBehaviour {
     public LayerMask whatIsGround;
 
     private bool moving;
-    
+
     // Stair logic
-    public float maxStepHeight = 0.4f;        // The maximum a player can set upwards in units when they hit a wall that's potentially a step
-    public float stepSearchOvershoot = 0.01f; // How much to overshoot into the direction a potential step in units when testing. High values prevent player from walking up tiny steps but may cause problems.
+    public float
+        maxStepHeight =
+            0.4f; // The maximum a player can set upwards in units when they hit a wall that's potentially a step
+
+    public float
+        stepSearchOvershoot =
+            0.01f; // How much to overshoot into the direction a potential step in units when testing. High values prevent player from walking up tiny steps but may cause problems.
+
+    private List<ContactPoint> allCPs = new List<ContactPoint>();
+    private Vector3 lastVelocity;
 
     public float counterMovement = 0.175f;
     private float threshold = 0.01f;
@@ -113,6 +122,8 @@ public class PlayerMovement : MonoBehaviour {
     }
 
     private void Movement() {
+        Vector3 _velocity = rb.velocity;
+
         // Air friction
         rb.velocity -= rb.velocity * airFriction;
 
@@ -153,9 +164,16 @@ public class PlayerMovement : MonoBehaviour {
         // Movement while sliding
         if (grounded && crouching) multiplierV = 0f;
 
+        Vector3 velf = orientation.transform.forward;
+        Vector3 velr = orientation.transform.right;
+        if (grounded && !jumping) {
+            velf = Vector3.ProjectOnPlane(orientation.transform.forward, normalVector);
+            velr = Vector3.ProjectOnPlane(orientation.transform.right, normalVector);
+        }
+
         //Apply forces to move player
-        rb.AddForce(orientation.transform.forward * (actualY * moveSpeed * Time.deltaTime * multiplier * multiplierV));
-        rb.AddForce(orientation.transform.right * (actualX * moveSpeed * Time.deltaTime * multiplier));
+        rb.AddForce(velf * (actualY * moveSpeed * Time.deltaTime * multiplier * multiplierV));
+        rb.AddForce(velr * (actualX * moveSpeed * Time.deltaTime * multiplier));
 
         // Play sound effects
         if ((!isEqual(x, 0) || !isEqual(y, 0)) && grounded) {
@@ -166,22 +184,42 @@ public class PlayerMovement : MonoBehaviour {
         }
 
         if (moving) AudioManager.instance.playFootstep();
-        
-        // I have no fucking idea why but clamp small movements
-         if (Math.Abs(rb.velocity.x) < 0.0) {
-             rb.velocity = new Vector3(0, rb.velocity.y, rb.velocity.z);
-         }
-         if (Math.Abs(rb.velocity.y) < 0.01) {
-             rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
-         }
-         if (Math.Abs(rb.velocity.z) < 0.01) {
-             rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, 0);
-         }
+
+        //I have no fucking idea why but clamp small movements
+        if (Math.Abs(rb.velocity.x) < 0.01) {
+            rb.velocity = new Vector3(0, rb.velocity.y, rb.velocity.z);
+        }
+
+        if (Math.Abs(rb.velocity.y) < 0.01) {
+            rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+        }
+
+        if (Math.Abs(rb.velocity.z) < 0.01) {
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, 0);
+        }
+
         if (grounded && !jumping) {
             rb.velocity = Vector3.ProjectOnPlane(rb.velocity, normalVector);
         }
 
-        Debug.Log($"{grounded}, {moving}");
+        //Filter through the ContactPoints to see if we're grounded and to see if we can step up
+        bool areWeGrounded = FindGround(out var groundCP, allCPs);
+
+        Vector3 stepUpOffset = default;
+        bool stepUp = false;
+        if (areWeGrounded)
+            stepUp = FindStep(out stepUpOffset, allCPs, groundCP, rb.velocity);
+
+        //Steps
+        if (stepUp) {
+            rb.position += stepUpOffset;
+            rb.velocity = lastVelocity;
+        }
+
+        allCPs.Clear();
+        lastVelocity = _velocity;
+
+        Debug.Log($"{grounded}, {moving}, {normalVector}, {lastVelocity}, STDATA {stepUp}, {areWeGrounded}");
     }
 
     public static bool isEqual(float f1, float f2) {
@@ -240,19 +278,9 @@ public class PlayerMovement : MonoBehaviour {
             return;
         }
 
-        //Counter movement
-        // if (Math.Abs(mag.x) > threshold && Math.Abs(x) < 0.05f || (mag.x < -threshold && x > 0) ||
-        //     (mag.x > threshold && x < 0)) {
-        //     rb.AddForce(orientation.transform.right * (moveSpeed * Time.deltaTime * -mag.x * counterMovement));
-        // }
-        //
-        // if (Math.Abs(mag.y) > threshold && Math.Abs(y) < 0.05f || (mag.y < -threshold && y > 0) ||
-        //     (mag.y > threshold && y < 0)) {
-        //     rb.AddForce(orientation.transform.forward * (moveSpeed * Time.deltaTime * -mag.y * counterMovement));
-        // }
-        // if (grounded) {
-        //     rb.velocity -= rb.velocity * friction;
-        // }
+        if (grounded) {
+            rb.velocity -= rb.velocity * friction;
+        }
 
         //Limit diagonal running. This will also cause a full stop if sliding fast and un-crouching, so not optimal.
         if (Mathf.Sqrt((Mathf.Pow(rb.velocity.x, 2) + Mathf.Pow(rb.velocity.z, 2))) > maxSpeed) {
@@ -292,22 +320,54 @@ public class PlayerMovement : MonoBehaviour {
     /// <summary>
     /// Handle ground detection
     /// </summary>
-    void OnCollisionStay(Collision other) {
+    void OnCollisionStay(Collision col) {
+        // Add all contacts
+        allCPs.AddRange(col.contacts);
+
         //Make sure we are only checking for walkable layers
-        int layer = other.gameObject.layer;
+        int layer = col.gameObject.layer;
         if (whatIsGround != (whatIsGround | (1 << layer))) return;
 
-        //Iterate through every collision in a physics update
-        for (int i = 0; i < other.contactCount; i++) {
-            Vector3 normal = other.contacts[i].normal;
+        bool isOnGround = false;
+        List<Vector3> normals = new List<Vector3>();
+        // Iterate through every collision in a physics update
+        // If all normals are up, we are on ground; if at least one normal is to the sid, it is a fucking slope
+        // In that case, just use the not-upwards one.
+        for (int i = 0; i < col.contactCount; i++) {
+            Vector3 normal_ = col.contacts[i].normal;
+            //Debug.DrawLine(col.contacts[i].point, col.contacts[i].point + col.contacts[i].normal, Color.green, 2,
+            //    false);
+
             //FLOOR
-            if (IsFloor(normal)) {
-                grounded = true;
-                cancellingGrounded = false;
-                normalVector = normal;
-                rb.useGravity = false;
-                CancelInvoke(nameof(StopGrounded));
+            if (IsFloor(normal_)) {
+                isOnGround = true;
             }
+
+            normals.Add(normal_);
+        }
+
+        bool isOnSlope = false;
+        Vector3 normal2 = default;
+        Vector3 tempNormal = default;
+        foreach (var normal in normals) {
+            if (normal != Vector3.up) {
+                isOnSlope = true;
+                normal2 = normal;
+            }
+
+            tempNormal = normal;
+        }
+
+        if (!isOnSlope) {
+            normal2 = tempNormal; // just set it to the last one fuck it
+        }
+
+        if (isOnGround) {
+            grounded = true;
+            cancellingGrounded = false;
+            normalVector = normal2;
+            rb.useGravity = false;
+            CancelInvoke(nameof(StopGrounded));
         }
 
         //Invoke ground/wall cancel, since we can't check normals with CollisionExit
@@ -318,15 +378,101 @@ public class PlayerMovement : MonoBehaviour {
         }
     }
 
-    void OnCollisionEnter(Collision other) {
-        //No chance to step if the player is not moving
-        if (moving) {
-            
-        }
+    void OnCollisionEnter(Collision col) {
+        allCPs.AddRange(col.contacts);
     }
 
     private void StopGrounded() {
         grounded = false;
         rb.useGravity = true;
+    }
+
+    /// Finds the MOST grounded (flattest y component) ContactPoint
+    /// \param allCPs List to search
+    /// \param groundCP The contact point with the ground
+    /// \return If grounded
+    bool FindGround(out ContactPoint groundCP, List<ContactPoint> allCPs) {
+        groundCP = default;
+        var found = false;
+        foreach (ContactPoint cp in allCPs) {
+            //Pointing with some up direction
+            if (cp.normal.y > 0.0001f && (found == false || cp.normal.y > groundCP.normal.y)) {
+                groundCP = cp;
+                found = true;
+            }
+        }
+
+        return found;
+    }
+
+    /// Find the first step up point if we hit a step
+    /// \param allCPs List to search
+    /// \param stepUpOffset A Vector3 of the offset of the player to step up the step
+    /// \return If we found a step
+    bool FindStep(out Vector3 stepUpOffset, List<ContactPoint> allCPs, ContactPoint groundCP, Vector3 currVelocity) {
+        stepUpOffset = default;
+
+        //No chance to step if the player is not moving
+        //if (!moving) return false;
+        if (Math.Abs(rb.velocity.x) < 0.01f) {
+            return false;
+        }
+
+        if (Math.Abs(rb.velocity.y) < 0.01f) {
+            return false;
+        }
+
+        if (Math.Abs(rb.velocity.z) < 0.01f) {
+            return false;
+        }
+
+        foreach (ContactPoint cp in allCPs) {
+            bool test = ResolveStepUp(out stepUpOffset, cp, groundCP);
+            if (test)
+                return test;
+        }
+
+        return false;
+    }
+
+    /// Takes a contact point that looks as though it's the side face of a step and sees if we can climb it
+    /// \param stepTestCP ContactPoint to check.
+    /// \param groundCP ContactPoint on the ground.
+    /// \param stepUpOffset The offset from the stepTestCP.point to the stepUpPoint (to add to the player's position so they're now on the step)
+    /// \return If the passed ContactPoint was a step
+    bool ResolveStepUp(out Vector3 stepUpOffset, ContactPoint stepTestCP, ContactPoint groundCP) {
+        stepUpOffset = default;
+        Collider stepCol = stepTestCP.otherCollider;
+
+        //( 1 ) Check if the contact point normal matches that of a step (y close to 0)
+        if (Mathf.Abs(stepTestCP.normal.y) >= 0.01f) {
+            return false;
+        }
+
+        //( 2 ) Make sure the contact point is low enough to be a step
+        if (!(stepTestCP.point.y - groundCP.point.y < maxStepHeight)) {
+            return false;
+        }
+
+        //( 3 ) Check to see if there's actually a place to step in front of us
+        //Fires one Raycast
+        RaycastHit hitInfo;
+        float stepHeight = groundCP.point.y + maxStepHeight + 0.0001f;
+        Vector3 stepTestInvDir = new Vector3(-stepTestCP.normal.x, 0, -stepTestCP.normal.z).normalized;
+        Vector3 origin = new Vector3(stepTestCP.point.x, stepHeight, stepTestCP.point.z) +
+                         (stepTestInvDir * stepSearchOvershoot);
+        Vector3 direction = Vector3.down;
+        if (!(stepCol.Raycast(new Ray(origin, direction), out hitInfo, maxStepHeight))) {
+            return false;
+        }
+
+        //We have enough info to calculate the points
+        Vector3 stepUpPoint = new Vector3(stepTestCP.point.x, hitInfo.point.y + 0.0001f, stepTestCP.point.z) +
+                              (stepTestInvDir * stepSearchOvershoot);
+        Vector3 stepUpPointOffset = stepUpPoint - new Vector3(stepTestCP.point.x, groundCP.point.y, stepTestCP.point.z);
+
+        //We passed all the checks! Calculate and return the point!
+        stepUpOffset = stepUpPointOffset;
+        return true;
     }
 }
